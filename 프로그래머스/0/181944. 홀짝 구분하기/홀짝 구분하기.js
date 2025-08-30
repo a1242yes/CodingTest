1e4ee94eb5d456c49f6ed581131dd32fc7265d68
a1242yes <150243473+a1242yes@users.noreply.github.com>
2025-08-30 23:27:23 +0900

    [level 0] Title: 홀짝 구분하기, Time: 44.86 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.js
@@ -0,0 +1,20 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    const n = Number(input[0]);
+    
+    // 짝수인지 홀수인지 판별
+    if (n % 2 === 0) {
+        console.log(`${n} is even`);
+    } else {
+        console.log(`${n} is odd`);
+    }
+});
